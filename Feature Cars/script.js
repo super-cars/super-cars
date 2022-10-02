3447d2536cb8baf55cdc8f280a53a109d543e8e0
vedantmistryy <44vedantmistry@gmail.com>
2022-10-02 15:29:18 +0530

    supercars website original

---
+++ Feature Cars/script.js
@@ -0,0 +1,22 @@
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () =>{
+document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+document.querySelector('.login-form-container').classList.remove('active');
+}
+
+
+
+
+
+
